302bc1fe399e0d0384c40c4b4ab05361bddd17be
niktos12 <nikitosss121207@gmail.com>
2024-03-25 18:32:46 +0400

    validation for creating product form

--- my-app/src/components/AddProduct.tsx
+++ my-app/src/components/AddProduct.tsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 import axios from "axios";
-import useStore from '../store';
+import useStore from "../store";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface AddProductModalProps {
   onClose: () => void;
 }
-
+const productSchema = z.object({
+  name: z.string().min(3, { message: "Title should be at least 3 characters" }),
+  price: z.number().min(1, { message: "Price should be at least 1" }),
+  image: z.string().url({ message: "Image should be a valid URL" }),
+  quantity: z.number().min(1, { message: "Quantity should be at least 1" }),
+  description: z
+    .string()
+    .min(5, { message: "Description should be at least 5 characters" }),
+});
+type IProduct = z.infer<typeof productSchema>;
 const AddProductModal: React.FC<AddProductModalProps> = ({ onClose }) => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
@@ -13,17 +25,27 @@ const AddProductModal: React.FC<AddProductModalProps> = ({ onClose }) => {
   const [image, setImage] = useState("");
   const [quantity, setQuantity] = useState(1);
   const { setProducts } = useStore();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<IProduct>({
+    resolver: zodResolver(productSchema),
+    defaultValues: {
+      name: "",
+      price: 0,
+      image: "",
+      description: "",
+      quantity: 1,
+    },
+  });
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    await axios.post('http://localhost:3001/products', { name, price , description, image, quantity });
-    const response = await axios.get('http://localhost:3001/products');
+  const onSubmit = async (data: IProduct) => {
+    await axios.post("http://localhost:3001/products", data);
+    const response = await axios.get("http://localhost:3001/products");
     setProducts(response.data);
-    setName('');
-    setPrice(0);
-    setDescription('');
-    setImage('');
-    setQuantity(1);
+    reset();
     onClose();
   };
 
@@ -31,7 +53,7 @@ const AddProductModal: React.FC<AddProductModalProps> = ({ onClose }) => {
     <>
       <div className="fixed bg-black/50 top-0 right-0 left-0 bottom-0 h-full"></div>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         className="bg-white rounded w-[500px] p-5 rounded bg-white z-40 left-1/2 -translate-x-1/2 fixed translate-y-1/2"
       >
         <h2 className="text-lg font-bold mb-4">Add Product</h2>
@@ -40,6 +62,7 @@ const AddProductModal: React.FC<AddProductModalProps> = ({ onClose }) => {
             Name
           </label>
           <input
+            {...register("name")}
             type="text"
             id="name"
             value={name}
@@ -47,12 +70,14 @@ const AddProductModal: React.FC<AddProductModalProps> = ({ onClose }) => {
             className="border p-2 w-full"
             required
           />
+          {errors.name && <p className="text-red-500">{errors.name.message}</p>}
         </div>
         <div className="mb-4">
           <label htmlFor="price" className="block text-gray-700 mb-2">
             Price
           </label>
           <input
+            {...register("price", { valueAsNumber: true })}
             type="number"
             id="price"
             value={price}
@@ -60,24 +85,32 @@ const AddProductModal: React.FC<AddProductModalProps> = ({ onClose }) => {
             className="border p-2 w-full"
             required
           />
+          {errors.price && (
+            <p className="text-red-500">{errors.price.message}</p>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="description" className="block text-gray-700 mb-2">
             Description
           </label>
           <textarea
+            {...register("description")}
             id="description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
             className="border p-2 w-full"
             required
           ></textarea>
+          {errors.description && (
+            <p className="text-red-500">{errors.description.message}</p>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="image" className="block text-gray-700 mb-2">
             Image
           </label>
           <input
+            {...register("image")}
             type="text"
             id="image"
             value={image}
@@ -85,12 +118,16 @@ const AddProductModal: React.FC<AddProductModalProps> = ({ onClose }) => {
             className="border p-2 w-full"
             required
           />
+          {errors.image && (
+            <p className="text-red-500">{errors.image.message}</p>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="quantity" className="block text-gray-700 mb-2">
             Quantity
           </label>
           <input
+            {...register("quantity", { valueAsNumber: true })}
             type="number"
             id="quantity"
             value={quantity}
@@ -98,6 +135,9 @@ const AddProductModal: React.FC<AddProductModalProps> = ({ onClose }) => {
             className="border p-2 w-full"
             required
           />
+          {errors.quantity && (
+            <p className="text-red-500">{errors.quantity.message}</p>
+          )}
         </div>
         <div className="flex justify-end">
           <button
